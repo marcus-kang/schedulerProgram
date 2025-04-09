6ba463df45b660543fe2b698d06d328489113008
marcus_kang <lebuet89@gmail.com>
2025-04-09 18:34:05 +0900

    customized Iterator added

--- SchedulerProgram/src/main/java/CustomizedPkg/MySingleLinkedList.java
+++ SchedulerProgram/src/main/java/CustomizedPkg/MySingleLinkedList.java
@@ -1,23 +1,64 @@
-package MyLinkedList;
+package CustomizedPkg;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class MySingleLinkedList<T> {
 
-	public Node<T> head;
-	public int size;
+	private Node<T> head;
+	private int size;
 	
 	public MySingleLinkedList() {
 		head = null;
 		size = 0;
 	}
 	
-	public void addFirst( T item ) {
+	//inner class
+	private static class Node<T> {
+		public T data;
+		public Node<T> next;
+		
+		public Node(T item) {
+			data = item;
+			next = null;
+		}
+	}
+	
+	public Iterator<T> iterator(){
+		return new MyIterator();
+	}
+	
+	private class MyIterator implements Iterator<T>{
+		private Node<T> nextNode;
+		public MyIterator() {
+			nextNode = head;
+		}
+		
+		public boolean hasNext() {
+			return (nextNode != null);
+		}
+		
+		public T next() {
+			if(nextNode == null)
+				throw new NoSuchElementException();
+			T val = nextNode.data;
+			nextNode = nextNode.next;
+			return val;
+		}
+		
+		public void remove() {
+			// ?
+		}
+	}
+	
+	private void addFirst( T item ) {
 		Node<T> newNode = new Node<T>(item);
 		newNode.next = head;
 		head = newNode;
 		size++;
 	}
 	
-	public void addAfter( Node<T> before, T item) {
+	private void addAfter( Node<T> before, T item) {
 		Node<T> temp = new Node<T>(item);
 		temp.next = before.next;
 		before.next = temp;
@@ -26,7 +67,7 @@ public class MySingleLinkedList<T> {
 	
 	public void add(int index, T item) { //insert
 		if(index < 0 || index >= size)
-			return;
+			throw new IndexOutOfBoundsException();
 		if(index == 0)
 			addFirst(item);
 		else {
@@ -35,7 +76,7 @@ public class MySingleLinkedList<T> {
 		}
 	}
 	
-	public T removeFirst() {
+	private T removeFirst() {
 		if(head == null)
 			return null;
 		T temp = head.data;
@@ -44,7 +85,7 @@ public class MySingleLinkedList<T> {
 		return temp;
 	}
 	
-	public T removeAfter(Node<T> before) {
+	private T removeAfter(Node<T> before) {
 		if(before.next == null)
 			return null;
 		T temp = before.next.data;
@@ -55,14 +96,14 @@ public class MySingleLinkedList<T> {
 	
 	public T remove(int index) { //delete
 		if(index < 0 || index >= size)
-			return null;
+			throw new IndexOutOfBoundsException();
 		if(index == 0)
 			return removeFirst();
 		Node<T> prev = getNode(index-1);
 		return removeAfter(prev);
 	}
 	
-	public T remove(T item) {
+	public boolean remove(T item) {
 		Node<T> p = head;
 		Node<T> q = null;
 		while(p!=null && !p.data.equals(item)) {
@@ -70,11 +111,15 @@ public class MySingleLinkedList<T> {
 			p = p.next;
 		}
 		if(p==null)
-			return null;
-		if(q==null)
-			return removeFirst();
-		else
-			return removeAfter(q);
+			return false;
+		if(q==null) {
+			T tmp = removeFirst();
+			return (tmp != null);
+		}
+		else {
+			T tmp = removeAfter(q);
+			return (tmp != null);
+		}
 	}
 	
 	public T get(int index) {
@@ -99,7 +144,7 @@ public class MySingleLinkedList<T> {
 		return -1;
 	}
 	
-	public Node<T> getNode(int index){
+	private Node<T> getNode(int index){
 		if(index < 0 || index >= size)
 			return null;
 		Node<T> p = head;
---
+++ SchedulerProgram/src/main/java/CustomizedPkg/Polynomial.java
@@ -0,0 +1,62 @@
+package CustomizedPkg;
+
+import java.util.Iterator;
+
+public class Polynomial {
+
+	public char name;
+	public MySingleLinkedList<Term> terms;
+	
+	public Polynomial(char name) {
+		this.name = name;
+		terms = new MySingleLinkedList<>();
+	}
+	
+	public void addTerm(int coef, int expo) {
+		if(coef == 0)
+			return;
+		
+//		Node<Term> p = terms.head, q = null;
+//		while(p != null && p.data.expo > expo) {
+//			q = p;
+//			p = p.next;
+//		}
+//		
+//		if(p != null && p.data.expo == expo) {
+//			p.data.coef += coef;
+//			if(p.data.coef == 0) {
+//				if(q == null)
+//					terms.removeFirst();
+//				else
+//					terms.removeAfter(q);
+//			}
+//		}else { //add after q
+//			Term t = new Term(coef, expo);
+//			if(q==null)
+//				terms.addFirst(t);
+//			else
+//				terms.addAfter(q, t);
+//		}
+	}
+	
+	public int calc(int x) {
+		int result = 0;
+		Iterator<Term> iter = terms.iterator();
+		while(iter.hasNext()) {
+			Term t = iter.next();
+			result += t.calc(x);
+		}
+		return result;
+	}
+	
+	public String toString() {
+		String result = "";
+		Iterator<Term> iter = terms.iterator();
+		while(iter.hasNext()) {
+			Term t = iter.next();
+			result += ("+" + t.toString());
+		}
+		return result;
+	}
+	
+}
